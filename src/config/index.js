b141b4ca4a96823427c07ead53573a49afcc0c09
tanzimul3islam <tanzimul3islam@gmail.com>
2024-11-07 12:48:42 +0600

    Added basic node express functionalitities

---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    ServerConfig: require('./server_config'),
+    Logger: require('./logger_config')
+}
---
+++ src/routes/v1/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const { InfoController } = require('../../controllers');
+
+const router = express.Router();
+
+router.get('/info', InfoController.info);
+
+module.exports = router;
+
+
+
+
+
+
